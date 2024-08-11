2c08d4190313a4759e5065fecb37e43cb9ecf6e9
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2024-08-11 10:54:38 +0100

    Initial Commit

---
+++ Tribe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tribe
+//
+//  Created by Vinicius Leal on 11/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tribe/TribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TribeApp.swift
+//  Tribe
+//
+//  Created by Vinicius Leal on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
